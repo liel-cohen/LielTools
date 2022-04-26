515d95f683d7d4e680a87293bdb31b645d11549a
eilay <eilay@post.bgu.ac.il>
2022-04-26 13:00:52 +0300

    תיקון שגיאה של ImportError: cannot import name '_centered' from 'scipy.signal.signaltools' (/home/oem/miniconda3/envs/lab_projects/lib/python3.9/site-packages/scipy/signal/signaltools.py)

--- StatsTools.py
+++ StatsTools.py
@@ -1,4 +1,14 @@
 import pandas as pd
+import  scipy.signal.signaltools
+def _centered(arr, newsize):
+    # Return the center newsize portion of the array.
+    newsize = np.asarray(newsize)
+    currsize = np.array(arr.shape)
+    startind = (currsize - newsize) // 2
+    endind = startind + newsize
+    myslice = [slice(startind[k], endind[k]) for k in range(len(endind))]
+    return arr[tuple(myslice)]
+scipy.signal.signaltools._centered = _centered
 import scipy
 import numpy as np
 import matplotlib.pyplot as plt
